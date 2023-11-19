b3e406ec6807f4986f5c7a96a20069ee67d37636
Olavi Mustanoja <tilastokeskus@gmail.com>
2023-11-19 22:08:17 +0200

    fix: spawn selections

--- MapsExtended.Editor/src/MapObjects/MapObjectPart.cs
+++ MapsExtended.Editor/src/MapObjects/MapObjectPart.cs
@@ -6,12 +6,16 @@ namespace MapsExt.Editor.MapObjects
 {
 	public sealed class MapObjectPart : MonoBehaviour
 	{
-		public Collider2D Collider { get; private set; }
+		public Collider2D Collider => this.GetComponent<Collider2D>();
 
 		private void Awake()
 		{
-			this.Collider = this.GetComponent<Collider2D>() ?? this.gameObject.AddComponent<BoxCollider2D>();
 			this.gameObject.GetOrAddComponent<MapObjectPartHandler>();
+
+			if (this.GetComponent<Collider2D>() == null)
+			{
+				this.gameObject.AddComponent<BoxCollider2D>();
+			}
 		}
 	}
 }
